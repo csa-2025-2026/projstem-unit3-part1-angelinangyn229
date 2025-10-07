e8edecc191b31a58eafc28a8f090579e430912c5
angelinangyn229 <angelinanguyen229@gmail.com>
2025-10-07 21:25:33 +0000

    jjn

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Rectangle r1 = new Rectangle(2.8, 5.4);
+    Rectangle r2 = new Rectangle(5.4, 2.8);
+
+    boolean widthSameLength = r1.getLength() == r2.getWidth();
+    boolean lengthSameWidth = g; r1.getWidth() == r2.getWidth();
+    boolean isRotated = lengthSameWidth && widthSameLength;
+
+    // Prob 1
+
+    boolean isCongruent = r1.equals(r2) || (isRotated == true);
+    // boolen isSimilar = (the rectangles are congruent) OR (the ratio of the lengths is equal to the ratio of the width)
+
+    double lengthRatio = (length of r1) / (length of r2);
+    double widthRatio = (width of r1) / (width of r2);
+    boolean equalRatio = lengthRatio == widthRatio;
+    boolean isSimilr = (isCongruent == true) || (equalRatio == true)
   }
 }
